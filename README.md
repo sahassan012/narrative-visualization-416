d9e25ab2b5fc9d361ba629d10242625850721bcc
Syed Hassan <syedhassan@Syeds-MacBook-Pro.local>
2022-07-21 20:52:03 -0500

    add tooltop and states mapping

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # narrative-visualization-416
+
+##Sources
+####TopoJSON: https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json
+
+#### COVID-19 Data: https://data.cdc.gov/NCHS/Provisional-COVID-19-Deaths-by-Sex-and-Age/9bhg-hcku
+
+####https://www.notion.so/Visualize-Data-with-a-Choropleth-Map-9d91d46e78d4406abc6a0d36f9e089dc
--- app.js
+++ app.js
@@ -1,283 +1,126 @@
-let worldcup_data;
-let filtered_data = [];
-let slideShowMapping = [1930, 1962, 1994, 2002, 2010];
-
-let svg;
-let xScaleBand, yScaleBand;
-let x, y;
-
-let year, away, home;
-let yearsList = new Set();
-let chosenYear = 1930;
-let chosenSide = "home";
-let chosenSlide = 0;
-let marginMapping;
-let height, width;
-
-//
-//	fetchPassengerData
-//
-//
-fetchPassengerData = function () {
-	d3.csv("world_cup_results_data.csv").then(function (response) {
-		worldcup_data = response;
-		initD3();
-		updateChart();
-		initDraggableTimeline();
-		updateSlideshow(0);
-	});
+let stateGeoJSONURL = "/US_geo.json";
+let COVIDdataURL = "/Provisional_COVID-19_Deaths_by_Sex_and_Age.csv";
+let statesMapping = {
+	01: "Alabama",
+	02: "Alaska",
+	04: "Arizona",
+	05: "Arkansas",
+	06: "California",
+	08: "Colorado",
+	09: "Connecticut",
+	10: "Delaware",
+	12: "Florida",
+	13: "Georgia",
+	15: "Hawaii",
+	16: "Idaho",
+	17: "Illinois",
+	18: "Indiana",
+	19: "Iowa",
+	20: "Kansas",
+	21: "Kentucky",
+	22: "Louisiana",
+	23: "Maine",
+	24: "Maryland",
+	25: "Massachusetts",
+	26: "Michigan",
+	27: "Minnesota",
+	28: "Mississippi",
+	29: "Missouri",
+	30: "Montana",
+	31: "Nebraska",
+	32: "Nevada",
+	33: "New Hampshire",
+	34: "New Jersey",
+	35: "New Mexico",
+	36: "New York",
+	37: "North Carolina",
+	38: "North Dakota",
+	39: "Ohio",
+	40: "Oklahoma",
+	41: "Oregon",
+	42: "Pennsylvania",
+	44: "Rhode Island",
+	45: "South Carolina",
+	46: "South Dakota",
+	47: "Tennessee",
+	48: "Texas",
+	49: "Utah",
+	50: "Vermont",
+	51: "Virginia",
+	53: "Washington",
+	54: "West Virginia",
+	55: "Wisconsin",
+	56: "Wyoming",
 };
 
-//
-//	initD3
-//
-//
-initD3 = function () {
-	var margin = { top: 30, right: 30, bottom: 70, left: 60 };
-	width = 1800 - margin.left - margin.right;
-	height = 600 - margin.top - margin.bottom;
+let statesData;
+let COVIDdata;
 
-	svg = d3
-		.select("#chart")
-		.append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+let svg = d3.select("#wrapper");
+var stateColor = d3.scaleLinear().domain([1, 99000]).range(["white", "red"]);
+let tooltip = d3.select("#tooltip");
 
-	xScaleBand = d3.scaleBand().range([0, width]).padding(0.2);
-	x = svg.append("g").attr("transform", "translate(0," + height + ")");
-
-	yScaleLinear = d3.scaleLinear().range([height, 0]);
-	y = svg.append("g");
-};
-
-//
-//	updateChart
-//
-//
-updateChart = function () {
-	updateButtons();
-	d3.select("#chart").selectAll("rect").remove();
-	for (let i = 0; i < worldcup_data.length; i++) {
-		var year = worldcup_data[i].Year;
-		if (!yearsList.has(year)) {
-			yearsList.add(year);
-		}
-	}
-
-	filtered_data = worldcup_data.filter((d) => d.Year == String(chosenYear));
-	filtered_data.sort((a, b) => {
-		if (chosenSide === "home") {
-			return a.HomeTeam > b.HomeTeam ? 1 : -1;
-		} else {
-			return a.AwayTeam > b.AwayTeam ? 1 : -1;
-		}
-	});
-
-	marginMapping = Array(yearsList.size)
-		.fill()
-		.map(function (_, idx) {
-			value = -30 + idx * 70;
-			return value;
-		});
-
-	xScaleBand.domain(
-		filtered_data.map(function (d) {
-			if (chosenSide === "home") {
-				return d.HomeTeam;
-			}
-			return d.AwayTeam;
-		})
-	);
-	x.transition().duration(1000).call(d3.axisBottom(xScaleBand));
-
-	yScaleLinear.domain([
-		0,
-		d3.max(filtered_data, function (d) {
-			if (chosenSide === "home") {
-				return d.HomeGoals;
-			}
-			return d.AwayGoals;
-		}),
-	]);
-	y.transition().duration(1000).call(d3.axisLeft(yScaleLinear));
-
-	let rect = svg.selectAll("rect").data(filtered_data);
-	rect
+let draw = function () {
+	svg
+		.selectAll("path")
+		.data(statesData)
 		.enter()
-		.append("rect")
-		.merge(rect)
-		.transition()
-		.duration(1000)
-		.attr("x", (d) =>
-			xScaleBand(chosenSide === "home" ? d.HomeTeam : d.AwayTeam)
-		)
-		.attr("y", (d) =>
-			yScaleLinear(chosenSide === "home" ? d.HomeGoals : d.AwayGoals)
-		)
-		.attr("width", xScaleBand.bandwidth())
+		.append("path")
+		.attr("d", d3.geoPath())
+		.attr("class", "state")
+		.attr("fill", (stateDataItem) => {
+			let id = stateDataItem["id"];
+			let data = COVIDdata.find((e) => {
+				return e["State"] === statesMapping[parseInt(id)];
+			});
+			let deaths = data ? parseInt(data["COVID-19 Deaths"]) : 0;
+
+			return stateColor(deaths);
+		})
 		.attr(
-			"height",
-			(d) =>
-				height - yScaleLinear(chosenSide === "home" ? d.HomeGoals : d.AwayGoals)
+			"data-state",
+			(stateDataItem) => statesMapping[parseInt(stateDataItem.id)]
 		)
-		.attr("fill", "#61adff");
-};
+		.attr("data-deaths", (stateDataItem) => {
+			let id = stateDataItem["id"];
+			let data = COVIDdata.find((e) => {
+				return e["State"] === statesMapping[parseInt(id)];
+			});
+			let deaths = data ? parseInt(data["COVID-19 Deaths"]) : 0;
+			return deaths;
+		})
+		.on("mouseover", (stateDataItem) => {
+			tooltip.transition().style("visibility", "visible");
+			let id = stateDataItem.target.__data__["id"];
+			let data = COVIDdata.find((e) => {
+				return e["State"] === statesMapping[parseInt(id)];
+			});
 
-//
-//	initDraggableTimeline
-//
-//
-initDraggableTimeline = function () {
-	initTimelineArrow();
-	initTimelineYears();
-};
-
-//
-//	initTimelineYears
-//
-//
-initTimelineYears = function () {
-	let timelineContainer = document.getElementById("timelineYears");
-
-	yearsList.forEach((value) => {
-		timelineContainer.innerHTML +=
-			"<div class='timeline-year' onclick='updateTimelineArrow(" +
-			value +
-			")'>" +
-			"<svg><circle cx='8' cy='8' r='8'/><svg>" +
-			"<p>" +
-			value +
-			"</p>" +
-			"</div>";
-	});
-};
-
-//
-//	initTimelineArrow
-//	 sources : https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_draggable
-//
-initTimelineArrow = function () {
-	let elmnt = document.getElementById("timelineArrow");
-	elmnt.style.marginLeft = "-30px";
-	var pos1 = 0,
-		pos2 = 0,
-		pos3 = 0,
-		pos4 = 0;
-	if (document.getElementById(elmnt.id + "Svg")) {
-		/* if present, the header is where you move the DIV from:*/
-		document.getElementById(elmnt.id + "Svg").onmousedown = dragMouseDown;
-	} else {
-		/* otherwise, move the DIV from anywhere inside the DIV:*/
-		elmnt.onmousedown = dragMouseDown;
-	}
-
-	function dragMouseDown(e) {
-		e = e || window.event;
-		e.preventDefault();
-		// get the mouse cursor position at startup:
-		pos3 = e.clientX;
-		pos4 = e.clientY;
-		document.onmouseup = closeDragElement;
-		// call a function whenever the cursor moves:
-		document.onmousemove = elementDrag;
-	}
-
-	function elementDrag(e) {
-		e = e || window.event;
-		e.preventDefault();
-		// calculate the new cursor position:
-		pos1 = pos3 - e.clientX;
-		pos2 = pos4 - e.clientY;
-		pos3 = e.clientX;
-		pos4 = e.clientY;
-		// set the element's new position:
-		var position = elmnt.offsetLeft - pos1;
-
-		// check for min and max position
-		let min = 147;
-		let max = 1477;
-		position = position < min ? min : position > max ? max : position;
-		elmnt.style.marginLeft = position + "px";
-	}
-
-	function closeDragElement() {
-		// check the closest year
-		position = parseInt(elmnt.style.marginLeft.slice(0, -2));
-		const indices = marginMapping.map((num) => {
-			return Math.abs(num - position);
+			tooltip.text(
+				data["State"] + " has " + data["COVID-19 Deaths"] + " deaths."
+			);
+			tooltip.attr("data-deaths", data["COVID-19 Deaths"]);
+		})
+		.on("mouseout", (stateDataItem) => {
+			tooltip.transition().style("visibility", "hidden");
 		});
-		let index = indices.indexOf(Math.min(...indices));
-		position = marginMapping[index];
-		elmnt.style.marginLeft = position + "px";
-		chosenYear = Array.from(yearsList)[index];
-		console.log(chosenYear);
+};
 
-		/* stop moving when mouse button is released:*/
-		document.onmouseup = null;
-		document.onmousemove = null;
+// read geojson for states and covid data from csv
+d3.json(stateGeoJSONURL).then(function (data, err) {
+	if (err) {
+		console.log(err);
+	} else {
+		statesData = topojson.feature(data, data.objects.states).features;
+		console.log(statesData);
+		d3.csv(COVIDdataURL).then(function (data, err) {
+			if (err) {
+				console.log(err);
+			} else {
+				COVIDdata = data;
+				console.log(COVIDdata);
+				draw();
+			}
+		});
 	}
-};
-
-//
-//	updateTimelineArrow
-//
-//
-updateTimelineArrow = function (year) {
-	let elmnt = document.getElementById("timelineArrow");
-	let index = [...yearsList].indexOf(String(year));
-	chosenYear = Array.from(yearsList)[index];
-	position = marginMapping[index];
-	elmnt.style.marginLeft = position + "px";
-	console.log(chosenYear);
-	updateChart();
-};
-
-//
-//	updateSide
-//
-//
-updateSide = function (side) {
-	if (chosenSide !== side) {
-		chosenSide = side;
-		updateChart();
-	}
-};
-
-//
-//	updateButtons
-//
-//
-updateButtons = function () {
-	let homeBtn = document.getElementById("homeBtn");
-	let awayBtn = document.getElementById("awayBtn");
-	if (chosenSide == "home" && !homeBtn.classList.contains("disabled")) {
-		homeBtn.classList.add("disabled");
-		awayBtn.classList.remove("disabled");
-	} else if (chosenSide == "away" && !awayBtn.classList.contains("disabled")) {
-		awayBtn.classList.add("disabled");
-		homeBtn.classList.remove("disabled");
-	}
-};
-
-//
-//	updateSlideshow
-//
-//
-updateSlideshow = function (slideshowIndex) {
-	let slideshowButtons = document.getElementsByClassName("slideshow-btn");
-	if (slideshowButtons[slideshowIndex].classList.contains("disabled")) {
-		return;
-	}
-	let year = slideShowMapping[slideshowIndex];
-	chosenSlide = slideshowIndex;
-	chosenYear = year;
-	updateTimelineArrow(year);
-
-	Array.from(slideshowButtons, (d) => {
-		d.classList.remove("disabled");
-	});
-	slideshowButtons[slideshowIndex].classList.add("disabled");
-	updateChart();
-};
+});
